f145c66fe2e60af8a34c285ff023a1e2dc9478ff
rifkikautsar <rifkikautsar1@gmail.com>
2022-12-05 17:13:24 +0700

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ResultController;
 use App\Http\Controllers\UploadController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\RegisterController;
@@ -18,8 +19,11 @@ use App\Http\Controllers\RegisterController;
 */
 
 Route::post('/uploads', [UploadController::class, 'index']);
-Route::post('/article', [ArticleController::class, 'index']);
+Route::post('/article/post', [ArticleController::class, 'index']);
 Route::get('/article', [ArticleController::class, 'all']);
+Route::get('/article/{key}', [ArticleController::class, 'getArticleById']);
 Route::post('/register', [RegisterController::class, 'index']);
 Route::post('/login', [LoginController::class, 'index']);
+Route::get('/results/{key}', [ResultController::class, 'index']);
+
 // Route::get("/activation?{key}", [UserController::class, 'activation']);
