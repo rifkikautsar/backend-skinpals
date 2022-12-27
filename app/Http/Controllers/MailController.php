db51da2933b478379feaf1f5efcd2a646b6c586e
rifkikautsar <rifkikautsar1@gmail.com>
2022-12-27 23:27:09 +0700

    create mail verification

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+class MailController extends Controller
+{
+    public function index($token, $to){
+        $details = [
+        'token' => $token 
+        ];
+        \Mail::to($to)->send(new \App\Mail\MyMail($details));
+        }
+}
