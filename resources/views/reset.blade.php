0fe4abd672df5afc29d6ae6b74a32966c69096ab
rifkikautsar <rifkikautsar1@gmail.com>
2023-01-23 22:10:14 +0700

    add views for reset pass

---
+++ resources/views/reset.blade.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title>Reset Password SkinPals</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style type="text/css">
+            /**
+   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
+   */
+            @media screen {
+                @font-face {
+                    font-family: "Source Sans Pro";
+                    font-style: normal;
+                    font-weight: 400;
+                    src: local("Source Sans Pro Regular"),
+                        local("SourceSansPro-Regular"),
+                        url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff)
+                            format("woff");
+                }
+                @font-face {
+                    font-family: "Source Sans Pro";
+                    font-style: normal;
+                    font-weight: 700;
+                    src: local("Source Sans Pro Bold"),
+                        local("SourceSansPro-Bold"),
+                        url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff)
+                            format("woff");
+                }
+            }
+            /**
+   * Avoid browser level font resizing.
+   * 1. Windows Mobile
+   * 2. iOS / OSX
+   */
+            body,
+            table,
+            td,
+            a {
+                -ms-text-size-adjust: 100%; /* 1 */
+                -webkit-text-size-adjust: 100%; /* 2 */
+            }
+            /**
+   * Remove extra space added to tables and cells in Outlook.
+   */
+            table,
+            td {
+                mso-table-rspace: 0pt;
+                mso-table-lspace: 0pt;
+            }
+            /**
+   * Better fluid images in Internet Explorer.
+   */
+            img {
+                -ms-interpolation-mode: bicubic;
+            }
+            /**
+   * Remove blue links for iOS devices.
+   */
+            a[x-apple-data-detectors] {
+                font-family: inherit !important;
+                font-size: inherit !important;
+                font-weight: inherit !important;
+                line-height: inherit !important;
+                color: inherit !important;
+                text-decoration: none !important;
+            }
+            /**
+   * Fix centering issues in Android 4.4.
+   */
+            div[style*="margin: 16px 0;"] {
+                margin: 0 !important;
+            }
+            body {
+                width: 100% !important;
+                height: 100% !important;
+                padding: 0 !important;
+                margin: 0 !important;
+            }
+            /**
+   * Collapse table borders to avoid space between cells.
+   */
+            table {
+                border-collapse: collapse !important;
+            }
+            a {
+                color: #1a82e2;
+            }
+            img {
+                height: auto;
+                line-height: 100%;
+                text-decoration: none;
+                border: 0;
+                outline: none;
+            }
+        </style>
+    </head>
+    <body style="background-color: #e9ecef">
+        <!-- start preheader -->
+        <div
+            class="preheader"
+            style="
+                display: none;
+                max-width: 0;
+                max-height: 0;
+                overflow: hidden;
+                font-size: 1px;
+                line-height: 1px;
+                color: #fff;
+                opacity: 0;
+            "
+        >
+            Kami menerima permintaan reset password dari akun anda.
+        </div>
+        <!-- end preheader -->
+
+        <!-- start body -->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <!-- start logo -->
+            <tr>
+                <td align="center" bgcolor="#f8dfc9">
+                    <table
+                        border="0"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        style="max-width: 600px"
+                    >
+                        <tr>
+                            <td
+                                align="center"
+                                valign="top"
+                                style="padding: 36px 24px"
+                            >
+                                <a
+                                    href="https://skinpals.id"
+                                    target="_blank"
+                                    style="display: inline-block"
+                                >
+                                    <img
+                                        src="https://api.skinpals.id/images/512px.png"
+                                        alt="Logo"
+                                        border="0"
+                                        width="48"
+                                        style="
+                                            display: block;
+                                            width: 64px;
+                                            max-width: 64px;
+                                            min-width: 48px;
+                                        "
+                                    />
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <!-- end logo -->
+
+            <!-- start hero -->
+            <tr>
+                <td align="center" bgcolor="#e9ecef">
+                    <table
+                        border="0"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        style="max-width: 600px"
+                    >
+                        <tr>
+                            <td
+                                align="left"
+                                bgcolor="#ffffff"
+                                style="
+                                    padding: 36px 24px 0;
+                                    font-family: 'Source Sans Pro', Helvetica,
+                                        Arial, sans-serif;
+                                    border-top: 3px solid #d4dadf;
+                                "
+                            >
+                                <h1
+                                    style="
+                                        margin: 0;
+                                        font-size: 32px;
+                                        font-weight: 700;
+                                        letter-spacing: -1px;
+                                        line-height: 48px;
+                                    "
+                                >
+                                    Reset Password - SkinPals
+                                </h1>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <!-- end hero -->
+
+            <!-- start copy block -->
+            <tr>
+                <td align="center" bgcolor="#e9ecef">
+                    <table
+                        border="0"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        style="max-width: 600px; table-layout: fixed"
+                    >
+                        <!-- start copy -->
+                        <tr>
+                            <td
+                                align="left"
+                                bgcolor="#ffffff"
+                                style="
+                                    padding: 24px;
+                                    font-family: 'Source Sans Pro', Helvetica,
+                                        Arial, sans-serif;
+                                    font-size: 16px;
+                                    line-height: 24px;
+                                "
+                            >
+                                <p style="margin: 0">
+                                    Kami menerima permintaan reset password dari
+                                    akun Anda. Jangan pernah memberitahukan
+                                    token ini kepada siapapun. Berikut token
+                                    untuk reset password:
+                                </p>
+                            </td>
+                        </tr>
+                        <!-- end copy -->
+
+                        <!-- start button -->
+                        <tr>
+                            <td align="left" bgcolor="#ffffff">
+                                <table
+                                    border="0"
+                                    cellpadding="0"
+                                    cellspacing="0"
+                                    width="100%"
+                                >
+                                    <tr>
+                                        <td
+                                            align="center"
+                                            bgcolor="#ffffff"
+                                            style="padding: 12px"
+                                        >
+                                            <table
+                                                border="0"
+                                                cellpadding="0"
+                                                cellspacing="0"
+                                            >
+                                                <tr>
+                                                    <td align="center">
+                                                        <p>{{ $details['token'] }}</p>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <!-- end button -->
+
+                        <!-- start copy -->
+                        <tr>
+                            <td
+                                align="left"
+                                bgcolor="#ffffff"
+                                style="
+                                    word-wrap: break-word;
+                                    padding: 24px;
+                                    font-family: 'Source Sans Pro', Helvetica,
+                                        Arial, sans-serif;
+                                    font-size: 16px;
+                                    line-height: 24px;
+                                "
+                            >
+                                <p style="margin: 0">
+                                    Token berlaku selama 1 jam. Jika Anda merasa tidak melakukan permintaan
+                                    reset password, abaikan Email ini.
+                                </p>
+                            </td>
+                        </tr>
+                        <!-- end copy -->
+
+                        <!-- start copy -->
+                        <tr>
+                            <td
+                                align="left"
+                                bgcolor="#ffffff"
+                                style="
+                                    padding: 24px;
+                                    font-family: 'Source Sans Pro', Helvetica,
+                                        Arial, sans-serif;
+                                    font-size: 16px;
+                                    line-height: 24px;
+                                    border-bottom: 3px solid #d4dadf;
+                                "
+                            >
+                                <p style="margin: 0">
+                                    Salam Sehat, Pals<br />
+                                    Tim SkinPals
+                                </p>
+                            </td>
+                        </tr>
+                        <!-- end copy -->
+                    </table>
+                </td>
+            </tr>
+            <!-- end copy block -->
+
+            <!-- start footer -->
+            <tr>
+                <td align="center" bgcolor="#f8dfc9" style="padding: 24px">
+                    <table
+                        border="0"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        style="max-width: 600px"
+                    >
+                        <!-- start permission -->
+                        <!-- end permission -->
+
+                        <!-- start unsubscribe -->
+                        <tr>
+                            <td
+                                align="center"
+                                bgcolor="#f8dfc9"
+                                style="
+                                    padding: 6px 8px;
+                                    font-family: 'Source Sans Pro', Helvetica,
+                                        Arial, sans-serif;
+                                    font-size: 14px;
+                                    line-height: 20px;
+                                    color: #666;
+                                "
+                            >
+                                <p style="font-size: 14px; line-height: 160%">
+                                    <span
+                                        style="
+                                            font-size: 20px;
+                                            line-height: 32px;
+                                        "
+                                        ><strong>Support Service</strong></span
+                                    >
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td
+                                align="center"
+                                bgcolor="#f8dfc9"
+                                style="
+                                    font-family: 'Source Sans Pro', Helvetica,
+                                        Arial, sans-serif;
+                                    font-size: 10px;
+                                    line-height: 8px;
+                                    color: #666;
+                                "
+                            >
+                                <p>
+                                    <span
+                                        style="
+                                            font-size: 12px;
+                                            line-height: 20px;
+                                        "
+                                        >support@skinpals.id</span
+                                    >
+                                </p>
+                            </td>
+                        </tr>
+                        <!-- end unsubscribe -->
+                        <tr>
+                            <td
+                                align="center"
+                                bgcolor="#f8dfc9"
+                                style="
+                                    padding: 12px 24px;
+                                    font-family: 'Source Sans Pro', Helvetica,
+                                        Arial, sans-serif;
+                                    font-size: 14px;
+                                    line-height: 20px;
+                                    color: #666;
+                                "
+                            >
+                                <p style="font-size: 14px; line-height: 180%">
+                                    <span
+                                        style="
+                                            font-size: 12px;
+                                            line-height: 28.8px;
+                                        "
+                                        >Copyrights &copy; SkinPals All Rights
+                                        Reserved</span
+                                    >
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <!-- end footer -->
+        </table>
+        <!-- end body -->
+    </body>
+</html>
